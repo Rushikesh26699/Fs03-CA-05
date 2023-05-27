ff5499de30550c1c5907487c65242ac76cb0fdda
Rushikesh Dhanawade <rushikesh26699@gmail.com>
2023-05-27 23:00:53 +0530

    Landing Page

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,14 @@
+import "./Landing.css"
+
+function Landing(){
+    return <>
+        <div className="Landing">
+           <div className="Tagline">
+           <h2 >Made With ❤️ By RUSHIKESH </h2>
+           </div>
+            
+        </div>
+    </>
+}
+
+export default Landing;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./Navbar.css"
+
+
+function Navbar(){
+    return <>
+
+    <div className="Navbar">
+      <div className="Nav1">
+        <h2>GEEKSTER</h2>
+      </div>
+      <div className="Nav2">
+        <button>HOME</button>
+        <button>ABOUT</button>
+        <button>MENU</button>
+        <button>ORDER</button>
+        <button>CONTACT</button>
+        
+      </div>
+   
+    </div>
+
+
+    </>
+}
+export default Navbar
